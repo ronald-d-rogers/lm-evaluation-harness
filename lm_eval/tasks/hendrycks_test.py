93084674bb3884d545dc81bcb45549d9fbbd21a9
Rogers, Ronald <rrogers@bloombergindustry.com>
2023-05-16 19:54:11 -0400

    fix: hendrycks test not downloading

--- lm_eval/tasks/hendrycks_test.py
+++ lm_eval/tasks/hendrycks_test.py
@@ -105,7 +105,7 @@ def create_task(subject):
 class GeneralHendrycksTest(MultipleChoiceTask):
     VERSION = 0
     DATASET_PATH = "mmlu"
-    DATASET_NAME = None
+    DATASET_NAME = "cais"
 
     def __init__(self, subject):
         self.DATASET_NAME = subject
